3b234b75da7da51ef959fc9460778af17409b310
vaibhav sukale <104653559+itsvaibhavcoder@users.noreply.github.com>
2023-10-12 01:00:07 +0530

    Create Inorder_traversal_in_binary_tree.cpp

---
+++ Leetcode Solutions/Inorder_traversal_in_binary_tree.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void helper(TreeNode* root, vector<int> &ans){
+        // base case
+        if(root==NULL) return;
+        helper(root->left, ans);
+        ans.push_back(root->val);
+        helper(root->right, ans);
+    }
+    
+vector<int> inorderTraversal(TreeNode* root) {
+    
+        vector<int> ans;
+        helper(root, ans);
+        return ans;
+        
+    }
+};
